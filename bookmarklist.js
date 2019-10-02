fe34d61188c81f835ea6a5f6d34e42b9ea63dab4
Heesu Student <hskang135@gmail.com>
2019-10-02 00:05:04 -0400

    fix some

--- bookmarklist.js
+++ bookmarklist.js
@@ -8,11 +8,11 @@ const render = function() {
         addNewBookmarkList()
     }
     else if(store.adding === false) {
-        renderList();
+        rendersList();
     }
 };
 
-const renderList = function() {
+const rendersList = function() {
     // render from bookmark store(array), render each one time
     const bookmarksHTML = store.bookmarks.map(bookmark => {
         return `
@@ -22,30 +22,64 @@ const renderList = function() {
       `}).join("") 
     
       $('some container').html(`<ul>${bookmarksHTML}</ul>`)
-}
+};
 
-const bookmarkDelete = function() {
+const addNewBookmarkList  = function() {
+    // add one by one
+    return (`
+    <form id="js-bookmark-list-form">
+        <div>
+            <label for="bookmark-list-entry">Bookmark Title</label>
+            <input type="text" name="bookmark-list-entry" class="js-bookmark-list-entry" placeholder="Google" required></input>
+        </div>
+        <div>
+            <label for="bookmark-list-entry">URL</label>
+            <input type="text" name="bookmark-list-entry" class="js-bookmark-list-entry" placeholder="https://www.google.com/" required></input>
+        </div>
+        <div>
+            <label for="bookmark-list-entry">Description</label>
+            <input type="text" name="bookmark-list-entry" class="js-bookmark-list-entry" placeholder="It's Google" required></input>
+        </div>
+        <div>
+            <label for="bookmark-list-entry">Rating</label>
+            <input type="number" name="bookmark-list-entry" class="js-bookmark-list-entry" 
+                placeholder="1-5" id="rate" min="1" max="5" required></input>
+        </div>
+            <button type="submit">Submit</button>
+    </form>
+    `)
+};
+
+const handleBookmarkDelete = function() {
     // delete one by one (similar shopping list)
-}
-
-const bookmarkAdd = function() {
-    // add one by one (similar shopping list)
-}
+    $('.js-bookmark-list').on('click', '.bookmark-item-delete', event => {
+        const id = getItemIdFromElement(event.currentTarget);
+        api.deleteBookmark(id)
+            .then(() => {
+                store.bookmarks.findAndDelete(id);
+                render();
+            })
+            .catch(error => {
+                store.bookmarks.setError(error.message);
+                render();
+            })
+    })
+};
 
 const bookmarkCheck = function() {
     // (similar shopping list)
+
 }
 
-const toggleView = function() {
+const generateBookmarkItems = function() {
     // expand or condense veiw, add html&url or not
-
-}
+    
+};
 
 export default {
     render,
     addNewBookmarkList,
-    bookmarkDelete,
-    bookmarkAdd,
+    handleBookmarkDelete,
     bookmarkCheck,
-    toggleView
+    generateBookmarkItems
 };
